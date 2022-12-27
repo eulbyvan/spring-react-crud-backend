cd0e1c463c817b5c7fb2ab64eee36e9b0d04c360
eulbyvan <work.issdq@gmail.com>
2022-12-27 14:21:45 +0700

    04-CRUD

--- src/main/java/com/eulbyvan/springreactcrudbackend/controller/UserController.java
+++ src/main/java/com/eulbyvan/springreactcrudbackend/controller/UserController.java
@@ -36,4 +36,24 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User newUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) throw new UserNotFoundException(id);
+        userRepository.deleteById(id);
+
+        return "User with id " + id + " has been deleted successfully";
+    }
 }
