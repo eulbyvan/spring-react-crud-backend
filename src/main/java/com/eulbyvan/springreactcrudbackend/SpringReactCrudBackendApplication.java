fcd4c69b54f16fe4034356af20b45bbdba7aec05
eulbyvan <work.issdq@gmail.com>
2022-12-27 10:01:36 +0700

    Initial Commit

---
+++ src/main/java/com/eulbyvan/springreactcrudbackend/SpringReactCrudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eulbyvan.springreactcrudbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactCrudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactCrudBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eulbyvan/springreactcrudbackend/SpringReactCrudBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eulbyvan.springreactcrudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactCrudBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
